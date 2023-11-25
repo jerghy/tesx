19d24ab3933824392522d20c80b92f08e1ad9bf0
jerghy <113653561+jerghy@users.noreply.github.com>
2023-11-25 17:28:45 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://d.pcs.baidu.com/file/5719cca34lde6cfe881563df29b6c4f2?fid=4160466670-250528-306141886068899&rt=pr&sign=FDtAERK-DCb740ccc5511e5e8fedcff06b081203-W0J8fA1HQCWTBGrwNAzdoXpoffY%3D&expires=8h&chkbd=0&chkv=0&dp-logid=944630657866084190&dp-callid=0&dstime=1700902391&r=824492936&vuk=4160466670&origin_appid=15195230&file_type=0"
+filename = "齐善鸿讲《道德经》.7z"
+
+response = requests.get(url, stream=True, headers={"User-Agent": "pan.baidu.com", "Cookie": "BDUSS=XdoZ2xKYUhhc3I1WE94Y3oxZVdlSDY1YUllMlVDbWRRZEJhcjRRejlVOEJKMEZsSVFBQUFBJCQAAAAAAAAAAAEAAADg9zH~ztLX7rrDNTYwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGaGWUBmhlle"})
+
+with open(filename, "wb") as file:
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            file.write(chunk)
+
+print("文件下载完成")
