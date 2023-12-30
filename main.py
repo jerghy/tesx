71bb51b96726b8ee392d2b85269bd694b5bb8aad
jerghy <113653561+jerghy@users.noreply.github.com>
2023-12-30 22:03:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
 
+# 指定下载的URL
 url = "https://desktop.figma.com/win/FigmaSetup.exe"
+# 设定要保存的文件名
 filename = "video.mp4"
 
+# 发起请求获取内容
 response = requests.get(url)
 
-with open(filename, "wb") as file:
-    for chunk in response.iter_content(chunk_size=1024):
-        if chunk:
-            file.write(chunk)
-
-print("文件下载完成")
+# 确保请求成功
+if response.status_code == 200:
+    # 打开文件进行写入
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    print(f"文件已下载并保存为 {filename}")
+else:
+    print("下载失败，状态码：", response.status_code)
